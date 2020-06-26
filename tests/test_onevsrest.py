cca597c367319e8d3e2ac5ed88a2eaf4fff9918a
navjordj <asd>
2020-06-26 15:53:37 +0200

    write onevsrest test

---
+++ tests/test_onevsrest.py
@@ -0,0 +1,28 @@
+import numpy as np
+from sklearn.multiclass import OneVsRestClassifier
+from sklearn import linear_model
+from sklearn import multiclass
+from src.onevsrest import OneVSRest
+from src.linear_model import LogisticRegression
+
+@pytest.mark.skip("Incorrect")
+def test_onevsrest():
+    X = np.array([
+        [10, 10],
+        [8, 10],
+        [-5, 5.5],
+        [-5.4, 5.5],
+        [-20, -20],
+        [-15, -20]
+    ])
+    y = np.array([0, 0, 1, 1, 2, 2])
+
+
+    sklearn_clf = multiclass.OneVsRestClassifier(linear_model.LogisticRegression()).fit(X, y)
+    sklearn_preds = sklearn_clf.predict(np.array([[-19, -20], [9, 9], [-5, 5]]))
+
+    scratch_clf = OneVSRest(LogisticRegression, epochs=100, learning_rate=0.01)
+    scratch_clf.fit(X, y)
+    scrath_preds = scratch_clf.predict(np.array([[-19, -20], [9, 9], [-5, 5]]))
+
+    assert (sklearn_preds == scrath_preds).all() 
