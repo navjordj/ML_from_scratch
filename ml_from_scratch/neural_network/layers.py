73a323edc28ccd040b72f301701b69ad84fb688c
navjordj <navjordj@gmail.com>
2020-06-28 15:15:25 +0200

    implement backprop in dense layer

--- ml_from_scratch/neural_network/layers.py
+++ ml_from_scratch/neural_network/layers.py
@@ -1,5 +1,6 @@
 
 import numpy as np 
+import copy
 
 
 class Layer():
@@ -20,18 +21,31 @@ class Dense(Layer):
         self.W = None
         self.b = None
 
-    def initialize(self):
+    def initialize(self, optimizer):
+        """Initializes the weights and biases and the optimizer 
+        """
         self.W = np.random.uniform(-1, 1, self.units) # TODO check limits
         self.b = np.zeros((1, self.units))
 
-    def forward(self):
+        self.W_optimizer = copy.copy(optimizer)
+        self.b_optimizer = copy.copy(optimizer)
+
+    def forward(self, X):
         """Forward pass through the dense layer.
         Calculated using a dotproduct between input and weights and add bias
         """
         self.layer_input = X
         return np.dot(X, self.W) + self.b
 
-    def backward(self):
+    def backward(self, accum_grad):
         """Backward pass through the Dense layer
+        Weights are updated using the optimizer
         """
-        raise NotImplementedError()
+        grad_w = self.layer_input.T.dot(accum_grad)
+        grad_b = np.sum(accum_grad, axis=0, keepdims=True)
+
+        self.W = self.W_optimizer.update(self.W, grad_w)
+        self.b = self.b_optimizer.update(self.b, grad_b)
+
+        accum_grad = accum_grad.dot(W.T)
+        return accum_grad
