694e822aa2bfb9522592971f4037e75b6118c80d
navjordj <navjordj@gmail.com>
2020-06-28 15:16:48 +0200

    implement  ReLU forward pass

--- ml_from_scratch/neural_network/activations.py
+++ ml_from_scratch/neural_network/activations.py
@@ -6,6 +6,11 @@ class Sigmoid():
         return 1 / (1 + np.exp(X))
 
 
+class ReLU():
+
+    def __call__(self, X):
+        return np.where(X >= 0, x, 0)
+
 if __name__ == "__main__":
     x = Sigmoid()
     print(x(0))
