1efddfa549414e9d2a3093bbd5d802ff59735e50
navjordj <navjordj@gmail.com>
2020-06-29 15:29:47 +0200

    implement backprop in relu and softmax

--- ml_from_scratch/neural_network/activations.py
+++ ml_from_scratch/neural_network/activations.py
@@ -9,7 +9,20 @@ class Sigmoid():
 class ReLU():
 
     def __call__(self, X):
-        return np.where(X >= 0, x, 0)
+        return np.where(X >= 0, X, 0)
+
+    def gradient(self, x):
+        return np.where(x >= 0, 1, 0)
+
+
+class Softmax():
+    def __call__(self, x):
+        e_x = np.exp(x - np.max(x, axis=-1, keepdims=True))
+        return e_x / np.sum(e_x, axis=-1, keepdims=True)
+
+    def gradient(self, x):
+        p = self.__call__(x)
+        return p * (1 - p)
 
 if __name__ == "__main__":
     x = Sigmoid()
