ac860be7e38bf16ae37768a9edf1fd31277fe7cb
navjordj <navjordj@gmail.com>
2020-06-29 15:29:14 +0200

    add one hot encoding function

---
+++ ml_from_scratch/helpers/encoding.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+def to_categorical(x, n_col=None):
+    """ One-hot encoding of nominal values """
+    if not n_col:
+        n_col = np.amax(x) + 1
+    one_hot = np.zeros((x.shape[0], n_col))
+    one_hot[np.arange(x.shape[0]), x] = 1
+    return one_hot
+
